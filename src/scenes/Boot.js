3b845898eb34d2097953c48cbdf2b14594257d88
Christian Dittrich <christian.dittrich@creatale.de>
2018-08-02 20:39:34 +0200

    fixed font loading

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -6,41 +6,38 @@ export default class extends Phaser.Scene {
 	constructor () {
 		super({ key: 'Boot' })
 	}
-
+	
 	initialize () {
 		this.stage.backgroundColor = '#EDEEC9'
 		this.fontsReady = false
-		this.fontsLoaded = this.fontsLoaded.bind(this)
 	}
-
+	
 	preload () {
 		if (config.webfonts.length) {
 			WebFont.load({
 				google: {
 					families: config.webfonts
 				},
-				active: this.fontsLoaded
+				active: this.fontsLoaded.bind(this)
 			})
 		}
-
+		
 		let text = this.add.text(config.width / 2, config.height / 2, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
 		text.setOrigin(0.5, 0.5)
-
+		
 		this.load.image('loaderBg', './assets/images/loader-bg.png')
 		this.load.image('loaderBar', './assets/images/loader-bar.png')
 	}
-
+	
 	update () {
-		this.scene.start('Splash')
-
-		if (config.webfonts.length && this.fontsReady) {
+		if (config.webfonts && config.webfonts.length > 0 && this.fontsReady) {
 			this.scene.start('Splash')
 		}
-		if (!config.webfonts.length) {
+		if (!config.webfonts || config.webfonts.length <= 0) {
 			this.scene.start('Splash')
 		}
 	}
-
+	
 	fontsLoaded () {
 		this.fontsReady = true
 	}
