94645bf98a59ad1ecb90fc218d0f730deb761074
Yann Pellegrini <mail@yann-p.fr>
2019-01-26 12:16:42 +0100

    Code formating

--- src/widgets/Memory.js
+++ src/widgets/Memory.js
@@ -1,8 +1,8 @@
 import { Collectable } from "./Collectable";
 
 export class Memory extends Collectable {
-  constructor ({ scene, x, y }) {
-		console.log('memory constructor scene')
-    super({ scene, x, y, key: 'memory' })
-	}
+  constructor({ scene, x, y }) {
+    console.log("memory constructor scene");
+    super({ scene, x, y, key: "memory" });
+  }
 }
