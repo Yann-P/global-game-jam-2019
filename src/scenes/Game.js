4726700cd6858207cb2d7a5d8353af77b83f0c65
Christian Dittrich <christian.dittrich@creatale.de>
2019-01-26 17:28:18 +0100

    removed obsolete elements

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -31,40 +31,6 @@ export default class extends Phaser.Scene {
 		for (let i = 0 ; i < 5; i++) {
 			this.collectableContainer.makeMemory({x: i * 100, y: 50});
 		}
-
-		const button = new TextButton({
-			scene: this,
-			text: 'Click Me!',
-			style: {
-				fontFamily: 'Bangers',
-				fontSize: 100,
-				color: '#ff0000'
-			},
-			x: this.sys.canvas.width / 2,
-			y: this.sys.canvas.height / 2,
-			width: 500,
-			height: 200,
-			onDown: () => {
-				store.dispatch('ressources/add', 100)
-			}
-		})
-		this.add.existing(button)
-		
-		const scoreText = new Phaser.GameObjects.Text(this, 0, 0, store.state.ressources.coins, {
-			fontSize: 100,
-			color: '#0000ff',
-			fontFamily: 'Bangers'
-		})
-		this.add.existing(scoreText)
-		
-		store.watch(
-			(state) => {
-				return state.ressources.coins
-			},
-			(newValue) => {
-				scoreText.setText(newValue)
-			}
-		)
 		
 		this.collectionJar = new CollectionJar({
 			scene: this,
