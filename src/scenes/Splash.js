e38921c7e579eb54af83e0e0a0f5e4e081f23efe
Yann Pellegrini <mail@yann-p.fr>
2019-01-27 14:49:42 +0100

    Paths

--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -19,35 +19,35 @@ export default class extends Phaser.Scene {
 		this.load.image('mushroom', 'assets/images/mushroom2.png')
 		this._makeMemoryTexture();
 
-		this.load.image('bg-end', './assets/images/bg-end.png')
-		this.load.image('finished', './assets/images/finished.png')
-		this.load.image('play', './assets/images/play.png')
-		this.load.image('gameover', './assets/images/gameover.png')
+		this.load.image('bg-end', 'assets/images/bg-end.png')
+		this.load.image('finished', 'assets/images/finished.png')
+		this.load.image('play', 'assets/images/play.png')
+		this.load.image('gameover', 'assets/images/gameover.png')
 
 
 		for(let i = 0; i <= 100; i += 10) {
-			this.load.image('e'+i, './assets/images/e'+i+'.png')
+			this.load.image('e'+i, 'assets/images/e'+i+'.png')
 
 		}
 
 
-		this.load.image('background', './assets/images/background.jpg')
-		this.load.image('pause', './assets/images/pause.png')
-		this.load.image('jar', './assets/images/jar.png')
-		this.load.image('jar-overlay', './assets/images/jar-overlay.png')
-		this.load.image('mem-good', './assets/images/mem-good.png')
-		this.load.image('mem-bad', './assets/images/mem-bad.png')
-		this.load.image('mem-trauma', './assets/images/mem-trauma.png')
-		this.load.image('alzheimer', './assets/images/alzheimer.png')
-		this.load.image('glow', './assets/images/glow.png')
+		this.load.image('background', 'assets/images/background.jpg')
+		this.load.image('pause', 'assets/images/pause.png')
+		this.load.image('jar', 'assets/images/jar.png')
+		this.load.image('jar-overlay', 'assets/images/jar-overlay.png')
+		this.load.image('mem-good', 'assets/images/mem-good.png')
+		this.load.image('mem-bad', 'assets/images/mem-bad.png')
+		this.load.image('mem-trauma', 'assets/images/mem-trauma.png')
+		this.load.image('alzheimer', 'assets/images/alzheimer.png')
+		this.load.image('glow', 'assets/images/glow.png')
 		this.load.image('startscreen', '/assets/images/Startscreen_MemoryLane_v002.png')
 		this.load.image('playButton', '/assets/images/PlayButton_MemoryLane_v001.png')
 
-		for(let i = 1; i <= 5; i++)	this.load.image('l'+i, './assets/images/l'+i+'.png')
+		for(let i = 1; i <= 5; i++)	this.load.image('l'+i, 'assets/images/l'+i+'.png')
 
-		this.load.image('progress-bg', './assets/images/progress-bg.png')
-		this.load.image('progress-fg', './assets/images/progress-fg.png')
-		this.load.image('progress-overlay', './assets/images/progress-overlay.png')
+		this.load.image('progress-bg', 'assets/images/progress-bg.png')
+		this.load.image('progress-fg', 'assets/images/progress-fg.png')
+		this.load.image('progress-overlay', 'assets/images/progress-overlay.png')
 	}
 
 	_makeMemoryTexture () {
